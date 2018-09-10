0e803e22c07f029840eada09568b65b2565df80c
chenlilun <chenlilun@myhexin.com>
2018-09-10 21:15:59 +0800

    Initial commit

---
+++ src/main/java/com/cll/myblog/controller/HelloController.java
@@ -0,0 +1,15 @@
+package com.cll.myblog.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by viruser on 2018/9/10.
+ */
+@RestController
+public class HelloController {
+    @RequestMapping("/hello")
+    public String index() {
+        return "Hello World!";
+    }
+}
